ee3dfdb552c65f81e90aa7a852b2a2d4bcdd2d20
JayPlay71 <jayaram.t.reddy@gmail.com>
2026-01-13 15:55:28 -0800

    Collision detection added

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -18,4 +18,9 @@ class Spaceship extends Phaser.GameObjects.Sprite {
             this.x = game.config.width;
         }
     }
+
+    // reset position
+    reset() {
+        this.x = game.config.width;
+    }
 }
