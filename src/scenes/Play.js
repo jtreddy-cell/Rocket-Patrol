a0f2b18937df109aee265247d01d0eb2a043ce70
JayPlay71 <jayaram.t.reddy@gmail.com>
2026-01-13 16:00:56 -0800

    Added explosion animation on collision

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -40,15 +40,15 @@ class Play extends Phaser.Scene {
         // check collisions
         if (this.checkCollision(this.p1Rocket, this.ship03)) {
             this.p1Rocket.reset();
-            this.ship03.reset();
+            this.shipExplode(this.ship03);
         }
         if (this.checkCollision(this.p1Rocket, this.ship02)) {
             this.p1Rocket.reset();
-            this.ship02.reset();
+            this.shipExplode(this.ship02);
         }
         if (this.checkCollision(this.p1Rocket, this.ship01)) {
             this.p1Rocket.reset();
-            this.ship01.reset();
+            this.shipExplode(this.ship01);
         }
     }
 
@@ -62,5 +62,18 @@ class Play extends Phaser.Scene {
         } else {
             return false;
         }
+    }
+
+    shipExplode(ship) {
+        // temporary hide ship
+        ship.alpha = 0;
+        // create explosion sprite at ship's position
+        let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0, 0);
+        boom.anims.play('explode');             // play explode animation
+        boom.on('animationcomplete', () => {    // callback after anim completes
+            ship.reset();                       // reset ship position
+            ship.alpha = 1;                     // make ship visible again
+            boom.destroy();                     // remove explosion sprite
+        });
     }
 }
